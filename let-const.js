87bf5ce00625d937db53cc80b9107d88c9777cfb
Faisal Wahab <someone@nowhere.com>
2020-03-08 02:35:55 +0600

    let-const.js

---
+++ let-const.js
@@ -0,0 +1,20 @@
+// value of a const variable can not be changed.
+// an array declared with const can be modified, update, change (like push(), pop()) but can't be declared again
+// let is another type of variable that can be changed or newly declared.
+// let it also called scope variable.
+
+/* 
+var is widely callable.
+Let is callable inside a {scope}
+*/
+
+
+const studentObj = {
+    name: "Sakib",
+    age: 27,
+    phone: 01729,
+    nationality: "BD"
+}
+
+
+console.log(studentObj);
